0fdf93bb9379c09884d4e1fdac8852998094a422
sindrtho <sindrtho@hotmail.com>
2019-03-04 10:28:17 +0100

    ...

--- SindreT/Oving3/Client.cpp
+++ SindreT/Oving3/Client.cpp
@@ -1,4 +1,60 @@
-//
-// Created by sindrtho on 3/1/19.
-//
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include <thread>
+#include "Kalkulator.cpp"
 
+using namespace boost::asio::ip;
+using namespace std;
+
+class EchoClient {
+    boost::asio::io_service io_service;
+    tcp::resolver resolver;
+
+public:
+    EchoClient(const std::string &host, unsigned short port) : resolver(io_service) {
+        auto query = tcp::resolver::query(host, to_string(port));
+
+        resolver.async_resolve(query, [this](const boost::system::error_code &ec,
+                tcp::resolver::iterator it) {
+            if(!ec) {
+                auto socket = make_shared<tcp::socket>(io_service);
+                boost::asio::async_connect(*socket, it, [this, socket](const boost::system::error_code &ec,
+                        tcp::resolver::iterator it) {
+                    if(!ec) {
+                        std::cout << "Connected" << std::endl;
+
+                        auto write_buffer = make_shared<boost::asio::streambuf>();
+                        ostream write_stream(write_buffer.get());
+
+                        write_stream << "Hello server!\r\n";
+                        cout << "Hello client!" << endl;
+                        async_write(*socket, *write_buffer, [this, socket, write_buffer](const boost::system::error_code &ec, size_t) {
+                            if (!ec) {
+                                auto read_buffer = make_shared<boost::asio::streambuf>();
+                                async_read_until(*socket, *read_buffer, "\r\n", [this, socket, read_buffer](
+                                         const boost::system::error_code &ec, size_t) {
+                                     if (!ec) {
+                                         cout << "From server: ";
+                                         std::string message;
+                                         istream read_stream(read_buffer.get());
+                                         while(getline(read_stream, message)) {
+                                             message.pop_back();
+
+                                             cout << message << std::endl;
+                                         }
+                                     }
+                                 });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+        io_service.run();
+    }
+};
+
+int main() {
+    EchoClient client("localhost", 80);
+}
--- SindreT/Oving3/Server.cpp
+++ SindreT/Oving3/Server.cpp
@@ -49,15 +49,15 @@ private:
 
                 if(params[0] == "add" && params.size() >= 3) {
                     try {
-                        write_stream << k.calculate(params[1], params[2], '+') << endl << endl;
+                        write_stream << k.calculate(params[1], params[2], '+') <<  "\r\n";
                     } catch(exception e) {
                         write_stream << "Invalid arguments" << endl;
                     }
                 } else if(params[0] == "sub" && params.size() >= 3) {
                     try {
-                        write_stream << k.calculate(params[1], params[2], '-') << endl << endl;
+                        write_stream << k.calculate(params[1], params[2], '-') << "\r\n";
                     } catch(exception e) {
-                        write_stream << "Invalid arguments" << endl;
+                        write_stream << "Invalid arguments" << "\r\n";
                     }
                 } else if(params[0] == "help") {
                     write_stream << helpMenu << endl;
@@ -85,8 +85,8 @@ private:
                 auto write_buffer = make_shared<boost::asio::streambuf>();
                 ostream write_stream(write_buffer.get());
 
-                write_stream << "Successfully connected to server." << endl;
-                write_stream << helpMenu << endl;
+//                write_stream << "Successfully connected to server.\r\n" << endl;
+                write_stream << helpMenu << "\r\n";
 
                 async_write(conn->sock, *write_buffer,
                             [this, conn, write_buffer](const boost::system::error_code &ec, size_t) {
